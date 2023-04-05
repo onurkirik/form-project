8bb5c4fec2a7eea7644606138b9634f323be3bcf
Onur Kırık <kirikonurr@gmail.com>
2023-04-05 22:16:11 +0300

    project has done

--- src/Components/TaskCreate.js
+++ src/Components/TaskCreate.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-function TaskCreate({ onCreate }) {
+function TaskCreate({ onCreate, task, taskFormUpdate }) {
 
-    const [title, setTitle] = useState('');
-    const [taskDesc, setTaskDesc] = useState('');
+    const [title, setTitle] = useState(task ? task.title : '');
+    const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : '');
 
     const handleChange = (event) => {
         setTitle(event.target.value);
@@ -14,7 +14,6 @@ function TaskCreate({ onCreate }) {
     }
 
     const handleSubmit = (event) => {
-
         // Formun submit olması durumunda sayfanın yenilenmesi olayını engelledik.
         event.preventDefault();
         onCreate(title, taskDesc);
@@ -23,17 +22,32 @@ function TaskCreate({ onCreate }) {
     }
 
     return (
-        <div className="task-create">
-            <h3>Lütfen Görev Ekleyiniz</h3>
-            <form className="task-form">
-                <label>Başlık</label>
-                <input value={title} onChange={handleChange} type="text" />
-                <label>Görev Giriniz!</label>
-                <textarea rows={5} onChange={handleTaskChange} ></textarea>
-                <button onClick={handleSubmit}>Oluştur</button>
-            </form>
+        <div>
+            {taskFormUpdate ? (
+                <div className="task-update">
+                    <h3>Lütfen Görev Düzenleyiniz</h3>
+                    <form className="task-form">
+                        <label>Başlığı düzenleyiniz</label>
+                        <input value={title} onChange={handleChange} type="text" />
+                        <label>Görev düzenleyiniz</label>
+                        <textarea rows={5} onChange={handleTaskChange} ></textarea>
+                        <button onClick={handleSubmit}>Düzenle</button>
+                    </form>
+                </div>
+            ) : (
+                <div className="task-create">
+                    <h3>Lütfen Görev Ekleyiniz</h3>
+                    <form className="task-form">
+                        <label>Başlık</label>
+                        <input value={title} onChange={handleChange} type="text" />
+                        <label>Görev Giriniz!</label>
+                        <textarea rows={5} onChange={handleTaskChange} ></textarea>
+                        <button onClick={handleSubmit}>Oluştur</button>
+                    </form>
+                </div>
+            )}
         </div>
-    );
+    )
 }
 
 export default TaskCreate;
--- src/Components/TaskShow.js
+++ src/Components/TaskShow.js
@@ -1,18 +1,33 @@
+import { useState } from "react";
+import TaskCreate from "./TaskCreate";
+
 function TaskShow({ task, onDelete }) {
+    const [showEdit, setShowEdit] = useState(false);
+
     const handleDeleteClick = () => {
         onDelete(task.id);
     };
 
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    }
+
     return (
         <div className="task-show">
-            <h3>Göreviniz</h3>
-            <p>{task.title}</p>
-            <h3>Yapılacaklar</h3>
-            <p>{task.taskDesc}</p>
-            <div>
-                <button onClick={handleDeleteClick} >Sil</button>
-                <button>Güncelle</button>
+
+            {showEdit ? (<TaskCreate task={task} taskFormUpdate={true} />) : (<div>
+                <h3>Göreviniz</h3>
+                <p>{task.title}</p>
+                <h3>Yapılacaklar</h3>
+                <p>{task.taskDesc}</p>
+                <div>
+                    <button onClick={handleDeleteClick} >Sil</button>
+                    <button onClick={handleEditClick}>Güncelle</button>
+                </div>
             </div>
+            )}
+
+
         </div>
     );
 }
